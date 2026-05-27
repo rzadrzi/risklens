009d2a890b4a64007958074669323cdf9427db5e
Reza Darzi <darzi.reza@gmail.com>
2026-05-27 15:40:07 +0330

    docs: Add discription for each function

--- risklens/risk/market_risk.py
+++ risklens/risk/market_risk.py
@@ -1,25 +1,33 @@
-# 01
-# daily returns
-# volatility
-# VaR
-# CVaR/ Expected Shortfall
-# Maximum Drawdown
 import numpy as np
 import pandas as pd
 
 def returns(prices: pd.DataFrame) -> pd.DataFrame:
+    """
+    return is a profit on an investment
+    :param prices:
+    :return: returns series
+    """
     return prices.pct_change().dropna()
 
 def log_returns(prices: pd.DataFrame) -> pd.DataFrame:
+    """
+    return is a profit on an investment
+    :param prices:
+    :return: returns series
+    """
     return np.log(prices/prices.shift(1)).dropna()
 
 def volatility(returns: pd.Series, annualized:bool=False, trading_days:int=252) -> float:
     """
+    Volatility shows how much a security or market index’s returns fluctuate over time,
+    indicating how widely prices move around their average.
+    It's often calculated from the standard deviation or the variance between those returns.
+    In most cases, the higher the volatility, the riskier the security.
 
     :param returns:
     :param annualized:
     :param trading_days:
-    :return:
+    :return: volatility
     """
     if returns.empty:
         raise ValueError("Returns series is empty.")
@@ -34,7 +42,8 @@ def volatility(returns: pd.Series, annualized:bool=False, trading_days:int=252) 
 
 def historical_VaR(returns: pd.Series, confidence_level:float=0.95) -> float:
     """
-    Calculate Historical Value at Risk(VaR).
+    It estimates how much a set of investments might lose (with a given probability),
+    given normal market conditions, in a set time period such as a day.
 
     :param returns: Asset returns series
     :param confidence_level: confidence level (default=0.95)
@@ -53,7 +62,10 @@ def historical_VaR(returns: pd.Series, confidence_level:float=0.95) -> float:
 
 def CVaR(returns: pd.Series, confidence_level:float=0.95)->float:
     """
-    Calculate Conditional VaR(CVaR).
+    Conditional Value-at-Risk (CVaR) is defined as a coherent risk measure that quantifies
+    the expected loss exceeding a specified value at a given confidence level.
+    It is also known as Mean Shortfall or Tail Var
+
     :param returns: Asset returns series
     :param confidence_level: confidence level (default=0.95)
     :return: Conditional VaR(CVaR)
@@ -72,6 +84,10 @@ def CVaR(returns: pd.Series, confidence_level:float=0.95)->float:
 
 def max_drawdown(returns: pd.Series)->float:
     """
+    The maximum drawdown, or “MDD”, is a metric that tracks
+    the most significant potential percentage decline in the value of a portfolio over a given period.
+    Conceptually, the maximum drawdown identifies the peak value and trough value of a portfolio or single investment,
+    i.e. the volatility risk.
 
     :param returns:
     :return:
